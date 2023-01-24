2b7449dc5fc91696dc41999d7f0f2018d6626dce
Nagimov Marat <123006710+pp2-22B030179@users.noreply.github.com>
2023-01-24 16:00:38 +0600

    week1

---
+++ TSIS1/variablenames.py
@@ -0,0 +1,15 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Camel Case
+myVariableName = "John"
+
+#Pascal case
+MyVariableName = "John"
+
+#Snake case
+my_variable_name = "John"
