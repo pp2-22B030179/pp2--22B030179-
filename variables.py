314e057d509cd3326f9084d99deeb095fe0671fc
Nagimov Marat <123006710+pp2-22B030179@users.noreply.github.com>
2023-01-24 13:39:40 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,30 @@
+#CREATING VARIABLES
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#CASTING
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#GET THE TYPE
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#SINGLE OR DOUBLE QUOTES?
+x = "John"
+# is the same as
+x = 'John'
+
+#CASE-SENSITIVE
+a = 4
+A = "Sally"
+#A will not overwrite a
